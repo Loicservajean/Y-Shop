fdec4ba06d3b9369e57f40a865dc52e9e9ecc4e8
CAILLOL <nicolas.caillol@free.fr>
2026-05-20 20:28:47 +0200

    feat/ jeux
    ajout des articles jeux

--- yshop/backend/app.js
+++ yshop/backend/app.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use('/images', express.static(path.join(__dirname, '../frontend/Image/FUMO')))
 app.use('/Mangas', express.static(path.join(__dirname, '../frontend/Image/Manga')))
-
+app.use('/Jeux', express.static(path.join(__dirname, '../frontend/Image/Jeux')))
 const productsRouter = require('./router/products')
 app.use(productsRouter)
 
